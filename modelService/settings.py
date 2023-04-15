a02c5a1b6e48cc6e7c1a940de476efed6d3a7c93
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-04-15 12:57:23 -0500

    Se permitió cualquier ip como host

--- modelService/settings.py
+++ modelService/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^8o9na&25$2fd)kczeu)c&6o*9(%jqd8kumsyzk-a25^dn($5n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
