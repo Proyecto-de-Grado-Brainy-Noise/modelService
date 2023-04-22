1569ccfc5ec7954b559472e2fa4398b7c54f5e67
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-04-22 12:31:09 -0500

    Corrección al procesar el json de metadata

--- model_prediction/tasks.py
+++ model_prediction/tasks.py
@@ -1,7 +1,6 @@
 import base64
 import json
 import os
-from celery import Celery
 import celery
 import nibabel as nib
 import os
@@ -15,64 +14,44 @@ from modelService.celery import app
 @app.task
 def process_nifti(niftiFile_base64,niftiFile_name,metadata_base64, email):
     try:   
+        BASE_VOLUME_PATH='/usr/src/app'
         
-        print("TASK ID: ",celery.current_task.request.id)
-        
-        print(f"WORKER CONNECTED TO VIEW!!!!!:{timezone.localtime(timezone.now())}")
+        print(f"Worker-Task started at: {timezone.localtime(timezone.now())}")
                 
         nifti_file_contents = base64.b64decode(niftiFile_base64.encode('utf-8'))
         
         metadata_file_contents = base64.b64decode(metadata_base64.encode('utf-8'))
+                
+        nifiti_file_temp_path=f"{BASE_VOLUME_PATH}/temp/{niftiFile_name[:8]}.nii.gz"
         
-        nifiti_file_temp_path=f"model_prediction/temp/{niftiFile_name[:8]}.nii.gz"
-        
-        metadata_file_temp_path=f"model_prediction/temp/{niftiFile_name[:8]}_metadata.json"
-        
-        print("Nifti route: ",nifiti_file_temp_path)
-        
-        print("Metadata route: ",metadata_file_temp_path)
-        
-        print("OKKKKKK")
-  
-        print("OKKKKKK 2.0")
         
         with open(nifiti_file_temp_path, "wb") as f:
             f.write(nifti_file_contents)
             
-            
-        with open(metadata_file_temp_path, "wb") as f:
-            f.write(metadata_file_contents)
-            
-                
+        file_contents= json.loads(metadata_file_contents)
+        
+                            
         nifti = nib.load(nifiti_file_temp_path)
         data = nifti.get_fdata()
         print("Nifti file shape: ",data.shape)
                 
-        print("DATA REMOVED!")
-        
+                
         cnn_model.model.summary()
-        print("NOW ITS TIME TO PREDICT THE DATA!")
+        print("Generating CNN prediction")
         predicted_label=cnn_model.getPrediction(data)
         print("Label: ",predicted_label)
-        
-                
-        with open(metadata_file_temp_path, 'r') as f:
-            file_contents = json.load(f)
             
         splitFileName=niftiFile_name.split(".")
-        
-                
+                        
         print("Uploading result to database...")
         ResonanceResult.objects.create(task_id=f"{celery.current_task.request.id}",predicton=predicted_label, metadata=file_contents, 
                                        fileName=splitFileName[0],email=email,
                                        fileExtension=".nii.gz",completeFileName=niftiFile_name,
                                        predicton_date=f"{timezone.localtime(timezone.now())}")
         
+        
         print("Deleting data...")
-        
         os.remove(nifiti_file_temp_path)
-        os.remove(metadata_file_contents)
-        
         
         return "Ok"
     except Exception as e:
--- model_prediction/views.py
+++ model_prediction/views.py
@@ -1,4 +1,3 @@
-import os
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,13 +6,16 @@ from rest_framework import status
 from .tasks import process_nifti
 import base64
 
-#from bson.objectid import ObjectId
 
 
 # Create your views here.
 
-
-#from model_prediction.models import ResonanceResult
+def showIntro():
+    print("+++++------------------------------------------------------------------------------------------------+++++")
+    print("+++++-------------------------------BRAINY NOISE CNN MODEL SERVICE-----------------------------------+++++")
+    print("+++++------------------------------------------------------------------------------------------------+++++")
+    print("+++++--------------------------Jessica N. , Estefanía B. , Sebastián V. -----------------------------+++++")
+    print("+++++------------------------------------------------------------------------------------------------+++++") 
 
 
 @csrf_exempt
@@ -21,12 +23,10 @@ import base64
 def makePrediction(request):
     
     try:       
-        dag_path=os.getcwd()
-        print("++------------------------------------------------------------------------+++++")
-        print(f"Getting file info: {timezone.localtime(timezone.now())}")
-        print("MAIN PATH: ",dag_path)
-        print(request.data)
-        
+        showIntro()
+        print(f"Getting file info at: {timezone.localtime(timezone.now())}")
+   
+   
         if 'resonance_file' not in request.data or 'metadata' not in request.data or 'email' not in request.data:
             return Response({'error': 'No data uploaded, some of the data is missing.'}, status=status.HTTP_400_BAD_REQUEST)
         
@@ -35,9 +35,9 @@ def makePrediction(request):
         email =  request.data['email']
         
         if not nifiti_file_obj.name.endswith('.nii.gz') or not metadata_file_obj.name.endswith('.json'):
-            print("Incorrect File 2")
             return Response({'error': 'Files must be a NIFTI and a JSON.'}, status=status.HTTP_400_BAD_REQUEST)
     
+        
     
         nifiti_file_contents = nifiti_file_obj.read()
         metadata_file_contents = metadata_file_obj.read()
@@ -47,12 +47,12 @@ def makePrediction(request):
         
         metadata_file_contents_b64 = base64.b64encode(metadata_file_contents).decode('utf-8')
         
-        print("Processing a queueing Nifti File...")
+        print("Processing and adding to queue Nifti File...")
             
         result=process_nifti.delay(nifiti_file_contents_b64,
                                    nifiti_file_obj.name,
                                    metadata_file_contents_b64,
-                                   email
+                                   email,
                                    )
             
         return Response({'message': 'NIFTI file uploaded successfully', "task_id":result.id},status=status.HTTP_200_OK)
@@ -62,42 +62,3 @@ def makePrediction(request):
     
     
         
-    
-
-    
-
-    # results = ResonanceResult.objects.all()
-    # data = [{'name': r.name, 'age': r.age, 'id': r._id} for r in results]
-    # print(data)
-
-
-
-
-
-
-
-#   result = ResonanceResult.objects.get(_id=ObjectId("6431dfb5b439ae1687706b1e"))
-#         print(result._id)
-
-
-
- # result=ResonanceResult.objects.all()
-        
-        # print("The length of the result is: ", len(result))
-        
-        
-        
-         
-        # result=ResonanceResult.objects.all()
-        
-        # print("The length of the result is: ", len(result))
-        
-        
-        # result = ResonanceResult.objects.get(_id=ObjectId("6433020fe5f52412a0a53625"))
-        # print(result._id)
-        
-        # ResonanceResult.objects.create(predicton=1, metadata={"hola":"no"}, 
-        #                            fileName="hola",
-        #                            fileExtension=".nii.gz",completeFileName="hola.nii.gz",
-        #                            predicton_date=f"{timezone.localtime(timezone.now())}")
-           
