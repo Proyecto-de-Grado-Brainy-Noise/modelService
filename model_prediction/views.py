c3f354ebc787bea1d30888ab15eb0dc49be81a5c
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-05-01 23:06:12 -0500

    Se corrigió la parte sobre tener metadata vacía

--- model_prediction/views.py
+++ model_prediction/views.py
@@ -1,3 +1,4 @@
+import json
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -5,7 +6,7 @@ from django.utils import timezone
 from rest_framework import status
 from .tasks import process_nifti
 import base64
-
+from  django.core.files.uploadedfile import TemporaryUploadedFile
 
 
 # Create your views here.
@@ -23,6 +24,7 @@ def showIntro():
 def makePrediction(request):
     
     try:       
+        
         showIntro()
         print(f"Getting file info at: {timezone.localtime(timezone.now())}")
    
@@ -33,9 +35,9 @@ def makePrediction(request):
         nifiti_file_obj = request.data['resonance_file']
         email =  request.data['email']
         
-        if('metadata' in request.data):
+        
+        if('metadata' in request.data) and  type(request.data['metadata'])==TemporaryUploadedFile:
             metadata_file_obj = request.data['metadata']
-            
             metadata_file_contents = metadata_file_obj.read()
             metadata_file_contents_b64 = base64.b64encode(metadata_file_contents).decode('utf-8')
         else:
@@ -61,7 +63,7 @@ def makePrediction(request):
             
         return Response({'message': 'NIFTI file uploaded successfully', "task_id":result.id},status=status.HTTP_200_OK)
         
-    except Exception as e:
+    except TypeError as e:
        return Response({'error':"Server error","description":e}, status=status.HTTP_400_BAD_REQUEST)
     
     
